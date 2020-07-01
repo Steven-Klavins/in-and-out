84494bf73b01c86b28c3b1bf007fb750145557fe
Steven <stevenklavins94@gmail.com>
2020-07-01 20:34:53 +0100

    feat: added test case box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Table from './Components/Table'
+import TestCase from './Components/TestCase'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Table />
+      <TestCase />
       <button className="plusButton">+</button>
     </div>
   )
