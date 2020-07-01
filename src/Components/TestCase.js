159bf34c2593b85d423965a31e5000d033f17f55
Steven <stevenklavins94@gmail.com>
2020-07-01 20:36:14 +0100

    feat: created test case component

---
+++ src/Components/TestCase.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TestCase.css'
+
+function TestCase() {
+  return (
+    <div className="testCase">
+      <p>In/Out</p>
+    </div>
+  )
+}
+
+export default TestCase
