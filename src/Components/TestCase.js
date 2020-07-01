8a8a80de14bf2da288c1c8a99be76abbdb0ae978
Steven <stevenklavins94@gmail.com>
2020-07-01 20:49:32 +0100

    chore: added input/output fields

--- src/Components/TestCase.js
+++ src/Components/TestCase.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import './TestCase.css'
 
-function TestCase() {
-  return (
-    <div className="testCase">
-      <p>In/Out</p>
-    </div>
-  )
+export class TestCase extends React.Component {
+  render() {
+    return (
+      <div className="testCase">
+        <form>
+          <label>
+            Input:
+            <input type="text" name="input" />
+          </label>
+
+          <label>
+            Exspected Output:
+            <input type="text" name="Output" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default TestCase
