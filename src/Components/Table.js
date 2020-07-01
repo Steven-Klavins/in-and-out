1c601970e0557669b3f7af8794aca4a44bfeb9c7
Steven <stevenklavins94@gmail.com>
2020-07-01 18:14:30 +0100

    chore: created table component file

---
+++ src/Components/Table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Table.css'
+
+function Table() {
+  return (
+    <div className="mainTable">
+      <h1>In & Out</h1>
+      <header></header>
+    </div>
+  )
+}
+
+export default Table
